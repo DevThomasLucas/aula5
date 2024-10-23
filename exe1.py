8adc36752c4ffa5bb1956bde9f012ea8eb9be44b
Thomas Lucas <t.lucas0504@gmail.com>
2024-10-22 21:25:47 -0300

    Att

--- exe1.py
+++ exe1.py
@@ -1,15 +1,16 @@
 "Apresentar os dois maiores valores de um vetor."
-x = [14,12,53,45,55,49,86,88,97,3,5]
-def exercico1 (x):
-    n = len(x)
-    ma1, ma2 = x[0], x[1]
-    if ma2 > ma1:
+x = [1, 2, 3, 3, 2, 1]
+
+
+n = len(x)
+ma1, ma2 = x[0], x[1]
+if ma2 > ma1:
         ma1, ma2 = ma2, ma1
-    for i in range(2,n):
-        if x[i]>ma2:
-            ma2 = x[i]
+        for i in range(2,n):
+            if x[i]>ma2:
+                ma2 = x[i]
             if ma2 > ma1:
                 ma1, ma2 = ma2, ma1
-    return ma1, ma2
+print (ma1, ma2)
 
 "limitante superior O(n)"
